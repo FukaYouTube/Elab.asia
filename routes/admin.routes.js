0d761e703145f8b86b2307fdbb8b78f237989c3c
Fuka <weendibol21300@gmail.com>
2020-08-13 21:50:00 +0600

    update

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -106,15 +106,27 @@ app.hears(/./gm, async (ctx, next) => {
                 message += StringParser.rules(messages['clients-info']['body-text'], { client }) + '\n'
             })
 
-            ctx.reply(message, inlineKeyboard(paginate(clients, 3, 1, clients.length / 3).keys.map(b => callbackButton(b.text, b.callback))).extra())
+            ctx.reply(message, inlineKeyboard(paginate(clients, 3, 1, (clients.length / 3 > Math.floor(clients.length / 3) ? Math.floor(clients.length / 3) + 1 : clients.length / 3)).keys.map(b => callbackButton(b.text, b.callback))).extra())
         break
         case messages.menu.buttons[0][1]:
             ctx.scene.enter('client-find-scene')
         break
         case messages.menu.buttons[1][0]:
-            ctx.scene.enter('news-scene')
+            let client_certificate = await User.find({ get_certificate: true })
+            
+            let msg = ''
+            for(client of client_certificate){
+                for(client_get_certificate of client.get_certificate_list){
+                    msg += StringParser.rules(messages['get-certificate'].text, { client, get_certificate_list: client_get_certificate })
+                }
+            }
+
+            ctx.reply(msg, keyboard(messages.menu.buttons).oneTime().resize().extra())
         break
         case messages.menu.buttons[2][0]:
+            ctx.scene.enter('news-scene')
+        break
+        case messages.menu.buttons[3][0]:
             ctx.reply(StringParser.rules(messages.help, { user: ctx.from }), keyboard(messages.menu.buttons).oneTime().resize().extra())
         break
         default: next()
@@ -143,11 +155,11 @@ app.action(/./gm, async (ctx, next) => {
     let clients = await User.find({})
     let message = messages['clients-info']['header-text'] + '\n'
 
-    paginate(clients, 3 , parseInt(ctx.callbackQuery.data), clients.length / 3).arr.map(client => {
+    paginate(clients, 3 , parseInt(ctx.callbackQuery.data), (clients.length / 3 > Math.floor(clients.length / 3) ? Math.floor(clients.length / 3) + 1 : clients.length / 3)).arr.map(client => {
         message += StringParser.rules(messages['clients-info']['body-text'], { client }) + '\n'
     })
 
-    ctx.reply(message, inlineKeyboard(paginate(clients, 3, parseInt(ctx.callbackQuery.data), clients.length / 3).keys.map(b => callbackButton(b.text, b.callback))).extra())
+    ctx.reply(message, inlineKeyboard(paginate(clients, 3, parseInt(ctx.callbackQuery.data), (clients.length / 3 > Math.floor(clients.length / 3) ? Math.floor(clients.length / 3) + 1 : clients.length / 3)).keys.map(b => callbackButton(b.text, b.callback))).extra())
 })
 
 module.exports = app
